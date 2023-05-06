8ec750d7d8f56f3a6a4437fe6a016c6c34ba53ae
EC2 Default User <ec2-user@ip-10-200-0-95.eu-north-1.compute.internal>
2023-05-06 03:44:46 +0000

    ddd

--- scripts/startnode.sh
+++ scripts/startnode.sh
@@ -2,7 +2,7 @@
 
 
 set -x
-sudo rm -rf node-modules/ package-lock.json
+sudo rm -rf package-lock.json
 sudo npm i
 sudo npm i pm2@latest -g
-sudo /usr/bin/pm2 start
+sudo /usr/bin/pm2 start index.js
