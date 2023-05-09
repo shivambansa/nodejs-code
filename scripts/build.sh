cc0d49ad4548fc80afca426cf63c23685ac25ada
awsandgit <aws4em@gmail.com>
2023-05-09 11:57:37 +0530

    changes to build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,4 +2,4 @@
 
 set -x
 mkdir build
-cp -fr index.js package.json ecosystem.config.js scripts/startnode.sh build/
+cp -fr index.js package-lock.json ecosystem.config.js scripts/startnode.sh build/
