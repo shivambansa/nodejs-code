523bb49dc76a2ae2fd93d0bd9e12d0a692fb0dd7
awsandgit <aws4em@gmail.com>
2023-05-09 12:17:15 +0530

    modified

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 8000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hi, changed some code. \n\n\nNode running at 8000! \n\n Deployed via ansible-playbook using jenkins.'
+  const msg = 'Hi, changed some code. \n\n\nNode running at 8000! \n\nDeployed via ansible-playbook using jenkins.'
   res.end(msg);
 });
 
