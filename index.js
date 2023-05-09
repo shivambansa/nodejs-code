37f108e4770b6b53dbd14d97f904ca4604089b2b
awsandgit <aws4em@gmail.com>
2023-05-09 12:13:47 +0530

    optimized startnode.sh

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 8000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Added another instance,application loadbalancer and cloudfront distro\nHello, Node running at 8000! \nChanges in code to see autodeployment'
+  const msg = 'Hi, changed some code. \n\n\nNode running at 8000! \n\n Deployed via ansible-playbook using jenkins.'
   res.end(msg);
 });
 
