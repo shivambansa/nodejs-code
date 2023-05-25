4e44e943f5169a99bf923e78bf16b9c847d96b2a
awsandgit <aws4em@gmail.com>
2023-05-25 11:06:24 +0530

    fix

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,5 +2,5 @@
 
 set -e
 cd /var/www/myapp/
-/usr/bin/pm2 reload mynodeapp || /usr/bin/pm2 start
+/usr/bin/pm2 reload mynodeapp &2>/dev/null || /usr/bin/pm2 start
 echo "Code ApplicationStart event script ran."
