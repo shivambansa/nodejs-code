883ba6fafd144edfdd400cf5885b01ae57f97788
awsandgit <aws4em@gmail.com>
2023-05-25 12:57:00 +0530

    fixedscript

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,5 +2,5 @@
 
 set -e
 cd /var/www/myapp/
-/usr/bin/pm2 reload mynodeapp &2>/dev/null || /usr/bin/pm2 start ecosystem.config.js
+/usr/bin/pm2 reload mynodeapp 2>/dev/null || /usr/bin/pm2 start ecosystem.config.js
 echo "Code ApplicationStart event script ran."
