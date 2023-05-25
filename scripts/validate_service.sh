81882e63212888cdcedd07733e89542902586124
awsandgit <aws4em@gmail.com>
2023-05-25 10:46:25 +0530

    edited

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -3,4 +3,5 @@
 set -e
 cd /var/www/myapp/
 /usr/bin/pm2 ls
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/bin/config.json -s
 echo "Code ValidateService event script ran."
