590d65122fde6bc74d8075f8cebba47bde0f8449
awsandgit <aws4em@gmail.com>
2023-06-13 11:29:36 +0530

    fixed script

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,10 +3,11 @@
 set -e
 cd /var/www/myapp/
 
-docker image rm nodeimage:latest 2> /dev/null
-docker build -t nodeimage .
-
 docker stop nodecontainer 2> /dev/null
 docker rm nodecontainer 2> /dev/null
+docker image rm nodeimage:latest 2> /dev/null
+
+docker build -t nodeimage .
 docker run -d -p 80:3000 --name nodecontainer nodeimage:latest
+
 echo "Code ApplicationStart event script ran."
