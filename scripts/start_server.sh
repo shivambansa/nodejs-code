c555deb91da6048bdc241af435f4b7fa71681ba8
awsandgit <aws4em@gmail.com>
2023-06-13 11:21:43 +0530

    docker pipe fixx

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -6,6 +6,7 @@ cd /var/www/myapp/
 docker image rm nodeimage:latest 2> /dev/null
 docker build -t nodeimage .
 
-docker stop nodecontainer 2> /dev/null && docker rm nodecontainer 2> /dev/null
+docker stop nodecontainer 2> /dev/null
+docker rm nodecontainer 2> /dev/null
 docker run -d -p 80:3000 --name nodecontainer nodeimage:latest
 echo "Code ApplicationStart event script ran."
